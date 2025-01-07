0f8443dff251d15628f5c7fb46bd68e7261bd2a5
Moriartyyy666 <fajrinugroho22@gmail.com>
2025-01-08 04:14:00 +0700

    fajri-views-login-page

--- resources/views/login-page.blade.php
+++ resources/views/login-page.blade.php
@@ -1,125 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
 <head>
-
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link rel="icon" type="image/x-icon" href="/assets/img/favicon.png">
-
-<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-
-<link rel="stylesheet" href="/assets/css/lnr-icon.css">
-
-<link rel="stylesheet" href="/assets/css/font-awesome.min.css">
-
-<link rel="stylesheet" href="/assets/css/style.css">
-<title>Login Page</title>
-
-<!--[if lt IE 9]>
-		<script src="assets/js/html5shiv.min.js"></script>
-		<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/lnr-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <title>{{ __('login.page_title') }}</title>
 </head>
 <body>
-
-
-
-<div id="loader-wrapper">
-<div class="loader">
-<div class="dot"></div>
-<div class="dot"></div>
-<div class="dot"></div>
-<div class="dot"></div>
-<div class="dot"></div>
-</div>
-</div>
-
-<div class="inner-wrapper login-body">
-<div class="login-wrapper">
-<div class="container">
-<div class="loginbox shadow-sm grow">
-<div class="login-left">
-<img class="img-fluid" src="/assets/img/logo.png" style="border-radius: 15%;" style="border-radius: 15%;" alt="Logo">
-</div>
-<div class="login-right">
-<div class="login-right-wrap">
-<h1>Login</h1>
-<p class="account-subtitle">Access to our dashboard</p>
-
-@if($errors->any())
-          			@foreach ($errors->all() as $error)
-          					<div id="errorBox" style="text-align:center;margin-top:20px;" class="alert alert-danger col-md-12 alert-dismissible fade show" role="alert">
-          							<strong style="color:white;">{!!$error!!}</strong>
-          							<button type="button" style="color:white;" class="close" data-dismiss="alert" aria-label="Close">
-          							<span aria-hidden="true" style="color:white;" >&times;</span>
-          							</button>
-          					</div>
-
-          					<script>
-
-          						window.onload=function(){
-
-          							$("#errorBox").delay(3000).fadeOut("slow");
-
-          						}
-
-          					</script>
-
-          			@endforeach
-          		@endif
-
-
-<form action="handle-login" method="POST" class="login-form">
-{{ csrf_field() }}
-<div class="form-group">
-<input class="form-control" type="text" placeholder="Username" name="username" required>
-</div>
-<div class="form-group">
-<input class="form-control" type="password" placeholder="Password" name="password" required>
-</div>
-
-<!--div class="form-group">
-<input type="radio" name="login_type" id="login_type_admin" value="admin">
-                <label for="inlineRadio1">Admin Login</label>
-
-                <input type="radio" name="login_type" id="login_type_staff" value="staff">
-                                    <label for="inlineRadio2">Staff Login</label>
-</div-->
-
-<div class="form-group">
-<button class="btn btn-theme button-1 text-white ctm-border-radius btn-block" type="submit" value="Login" >Login</button>
-</div>
-</form>
-
-<div class="text-center forgotpass"><a href="forgot-password.html">Forgot Password?</a></div>
-<div class="login-or">
-<span class="or-line"></span>
-</div>
-
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-
-
-
-<script src="/assets/js/jquery-3.2.1.min.js"></script>
-
-<script src="/assets/js/popper.min.js"></script>
-<script src="/assets/js/bootstrap.min.js"></script>
-
-<script src="/assets/plugins/theia-sticky-sidebar/ResizeSensor.js"></script>
-<script src="/assets/plugins/theia-sticky-sidebar/theia-sticky-sidebar.js"></script>
-
-<script src="/assets/js/script.js"></script>
+    <div class="loader-wrapper">
+        <div class="loader">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+    </div>
+    <main class="inner-wrapper login-body">
+        <section class="login-wrapper">
+            <div class="container">
+                <div class="loginbox shadow-sm grow">
+                    <div class="login-left">
+                        <img class="img-fluid rounded-logo" src="{{ asset('assets/img/logo.png') }}" alt="Logo of {{ config('app.name') }}">
+                    </div>
+                    <div class="login-right">
+                        <div class="login-right-wrap">
+                            <h1>{{ __('login.title') }}</h1>
+                            <p class="account-subtitle">{{ __('login.subtitle') }}</p>
+                            @if ($errors->any())
+                                <div id="errorBox" class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{!! $error !!}</li>
+                                        @endforeach
+                                    </ul>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close" role="button">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            @endif
+                            <form action="{{ route('handle-login') }}" method="POST" class="login-form">
+                                @csrf
+                                <div class="form-group">
+                                    <input class="form-control" type="text" placeholder="{{ __('login.username_placeholder') }}" name="username" required>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" type="password" placeholder="{{ __('login.password_placeholder') }}" name="password" required>
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-theme button-1 text-white ctm-border-radius btn-block" type="submit">{{ __('login.submit_button') }}</button>
+                                </div>
+                            </form>
+                            <div class="text-center forgotpass"><a href="{{ route('forgot-password') }}">{{ __('login.forgot_password') }}</a></div>
+                            <div class="login-or">
+                                <span class="or-line"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <script src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
 </body>
-
-
 </html>
 
-
+/* CSS */
+<style>
+    .rounded-logo {
+        border-radius: 15%;
+    }
+</style>
