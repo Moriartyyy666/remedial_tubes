417dfcd8c2f1fafa2007d59d2b519b9b09e28340
Moriartyyy666 <fajrinugroho22@gmail.com>
2025-01-08 12:43:25 +0700

    fajri-views-admindashcont-homepage

--- resources/views/admin-dashboard-content/home-page.blade.php
+++ resources/views/admin-dashboard-content/home-page.blade.php
@@ -1,46 +1,26 @@
 @extends('admin-dashboard-layout.dashboard-template')
 
 @section('dashboard-admin-content')
-
-
 <div class="card">
     <div class="card-body">
-      <h3 class="panel-title" style="text-align:center;">Pending Requests</h3>
-      <br>
-
-
-
-      @foreach ($pending_data as $key => $data)
-
-          <div class="card   mb-3">
-            <div class="card-header  ">
-              <strong>{{$data->date_of_leave}} ({{$data->firstname}} {{$data->lastname}})</strong>
-              <i class="float-right" style="font-size:85%;">Request sent on :- {{$data->date_of_request}}</i>
-            </div>
-            <div class="card-body">
-            
-
-             @foreach($leave_type as $keyy=>$object)
-            
-             @if($object->id== $data->type_of_leave) 
-             {{ $object->leave_type_name }}
-             @endif
-              @endforeach
-
-             
-              <a style="margin-left:10px;" class="btn btn-danger  float-right " href="/decline-request/{{$data->auto_id}}">Decline</a>
-              <a class="btn btn-primary float-right" href="/accept-request/{{$data->auto_id}}">Accept</a>
-
-            </div>
-          </div>
-
-      @endforeach
-
-
-
+        <h3 class="panel-title text-center">Pending Requests</h3>
+        <br>
+        @foreach ($pending_data as $key => $data)
+            <article class="card mb-3" aria-label="Pending Request for {{$data->firstname}} {{$data->lastname}}">
+                <div class="card-header">
+                    <strong>{{$data->date_of_leave}} ({{$data->firstname}} {{$data->lastname}})</strong>
+                    <i class="float-right small">Request sent on :- {{$data->date_of_request}}</i>
+                </div>
+                <div class="card-body">
+                    @php
+                        $leaveTypeName = $leave_type->firstWhere('id', $data->type_of_leave)->leave_type_name ?? 'Unknown';
+                    @endphp
+                    {{ $leaveTypeName }}
+                    <a class="btn btn-danger float-right ml-2" href="{{ route('request.decline', $data->auto_id) }}">Decline</a>
+                    <a class="btn btn-primary float-right" href="{{ route('request.accept', $data->auto_id) }}">Accept</a>
+                </div>
+            </article>
+        @endforeach
     </div>
 </div>
-
-
-
 @endsection
